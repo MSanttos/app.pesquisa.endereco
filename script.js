b7836b742b297c296f6fea691b06a5accd24372a
Michel Santos <michel.cxs@gmail.com>
2021-10-04 13:11:26 -0300

    adjusts

--- script.js
+++ script.js
@@ -10,8 +10,14 @@ function getDadosEnderecoPorCep(cep) {
   xmlHttp.onreadystatechange = () => {
     if(xmlHttp.readyState == 4 && xmlHttp.status == 200){
       console.log(xmlHttp.responseText)
+      let dadosJSONText = xmlHttp.responseText
+      let dadosJSONObj = JSON.parse( dadosJSONText)
+
+      document.getElementById('endereco').value = dadosJSONObj.logradouro
+      document.getElementById('bairro').value = dadosJSONObj.bairro
+      document.getElementById('cidade').value = dadosJSONObj.localidade
+      document.getElementById('uf').value = dadosJSONObj.uf
     }
   }
-
   xmlHttp.send()
 }
